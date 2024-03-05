7428c7a51db548fd6db9a46311335d1d00fe9eba
joeyissa <joeyissa59@gmail.com>
2024-03-04 22:12:09 -0500

    set up

--- histograms.rkt
+++ histograms.rkt
@@ -1,25 +1,57 @@
 #lang racket
 
 
+; Python file already reduces the histogram to 3 bit
+          
+(define (ColorImage filename)
+  (begin 
+    (define pixelImage (open-input-file filename))
+    (define coloredHist (ColorHisto pixelImage))
+    (save colorHist)
+    ; return coloredHist
+  )
+
+
+(define (ColorHisto pixelImage)
+  ; read the file that is opened (recursive)
+  ; make a vector of the first interger that occurs in the first list
+  ; set-vector ____
+  ; add the rest of the values into a vector
+  ; return the vector
+  )
+
+
+
+(define (save filename)
+  ; take the vector and each element and write it into a new txt file
+  )
+
+
+(define (compare hist1 hist2)
+  )
+
+
+(define (getSim queryHist)
+
+  )
+
+ (define (calls quertHist)
+   ; call color histogram and get the histogram)
+   ; call compare
+   ; (compare queryHist
+   )
+  
 (define (similaritySearch queryHistogramFilename imageDatasetDirectory)
   ; This function should return the name of the 5 most similar images to the query image
   ; this is where we will do all of the function calls
    (if (file-exists? queryHistogramFilename)
         (begin
-          (ColorImage fileName 3) ; define pixelImage (open-input-file
-          (
+          (define queryHist (ColorImage fileName)) ; define pixelImage (open-input-file
+          ; compare things
+          (getSim (queryHist imageDatasetDirectory))
   )
 
 
 
-
-(define (ColorImage filename reduceColor)
-  (begin 
-    (define pixelImage (open-input-file filename))
-    ; add these to a list
-
-    (define reduedImage (reduceColor List))
-    )
-  )
 
   
